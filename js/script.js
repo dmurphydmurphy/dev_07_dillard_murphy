f8d5f6aa1c4c87e331995086d63b708b94ccbf4c
dmurphy <55006323+dmurphydmurphy@users.noreply.github.com>
2019-10-24 21:22:33 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function (){
 $("#content").hide().fadeIn(3000);
   
 // ----------------------------------------------------------------------------
